f9ea0db8a1e510856a471df827d2c3530b9108d9
Yamagishi Kazutoshi <ykzts@desire.sh>
2017-05-28 14:30:28 +0900

    add localization test (#25)

--- __tests__/containers/Home.js
+++ __tests__/containers/Home.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { IntlProvider } from 'react-intl';
 import renderer from 'react-test-renderer';
 import Home from '../../src/containers/Home';
-import messages from '../../src/locales/en';
+import enMessages from '../../src/locales/en';
+import jaMessages from '../../src/locales/ja';
 
-test('Home container renders correctly', () => {
+test('Home container renders English messages', () => {
   const container = renderer.create((
-    <IntlProvider locale="en" messages={messages}>
+    <IntlProvider locale="en" messages={enMessages}>
+      <Home />
+    </IntlProvider>
+  ));
+  const tree = container.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Home container renders Japanese messages', () => {
+  const container = renderer.create((
+    <IntlProvider locale="ja" messages={jaMessages}>
       <Home />
     </IntlProvider>
   ));
